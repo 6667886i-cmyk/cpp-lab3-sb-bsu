fa873e29f0dab19a827ce7b141463b074aa379cc
6667886i-cmyk <6667886i@gmail.com>
2025-11-28 17:02:21 +0300

    Create 3a-t4.cpp

---
+++ 3a-t4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int k;
+cout << " Enter the amount of integers k: ";
+cin >> k;
+if (k == 0) {
+cout << "sum: 0" << endl; // Если k=0, сумма равна 0
+} else {
+int sum = 0, num;
+for (int i = 0; i < k; ++i) {
+cout << "Enter integer nr. " << i << ": ";
+cin >> num;
+sum += num;
+}
+cout << "sum: " << sum << endl;
+}
+return 0;
+}
